6bfc5222e467372c49101d038b6fae72b779fd13
Anton Dementyev <knoexus@yahoo.com>
2020-01-10 17:50:00 +0300

    image upload

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions')
 const app = require('express')()
 
 const { getAllScreams, postOneScream } = require('./handlers/screams')
-const { signUp, login } = require('./handlers/users')
+const { signUp, login, uploadImage } = require('./handlers/users')
 const FBAuth = require('./util/fbAuth')
 
 //scream routes
@@ -12,6 +12,7 @@ app.post('/scream', FBAuth, postOneScream)
 //auth routes
 app.post('/signup', signUp)
 app.post('/login', login)
+app.post('/user/image', FBAuth, uploadImage)
 
 
 exports.api = functions.https.onRequest(app)
