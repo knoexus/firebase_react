87fa294adea67c362452505298d313a86f1fcde3
Anton Dementyev <knoexus@yahoo.com>
2020-01-10 14:31:36 +0300

    fixed serviceaccount

--- dbschema.js
+++ dbschema.js
@@ -3,7 +3,9 @@ let db = {
         {
             userHandle: 'user',
             body: 'this is the scream body',
-            createdAt: '2020-01-09T14:27:55.464Z'
+            createdAt: '2020-01-09T14:27:55.464Z',
+            likeCount: 5,
+            commentCount: 2
         }
     ]
 }
