6f7070c721463bd16c3af29174bb41c1975879f1
Anton Dementyev <knoexus@yahoo.com>
2020-01-11 14:08:29 +0300

    crud operations with screams / errors unfixed

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 const functions = require('firebase-functions')
 const app = require('express')()
 
-const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams')
+const { getAllScreams, postOneScream, getScream, 
+        commentOnScream, likeScream, unlikeScream, deleteScream } = require('./handlers/screams')
 const { signUp, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users')
 const FBAuth = require('./util/fbAuth')
 
@@ -9,7 +10,10 @@ const FBAuth = require('./util/fbAuth')
 app.get('/screams', getAllScreams)
 app.post('/scream', FBAuth, postOneScream)
 app.get('/scream/:screamId', getScream)
+app.delete('/scream/:screamId', FBAuth, deleteScream)
 app.post('/scream/:screamId/comment', FBAuth, commentOnScream)
+app.get('/scream/:screamId/like', FBAuth, likeScream)
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream)
 
 //auth routes
 app.post('/signup', signUp)
